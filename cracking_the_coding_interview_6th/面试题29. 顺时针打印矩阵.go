276915e65b2a5b1965ce26365857c4da7bd9856b
kuhufu <huboemail@yeah.net>
2020-06-05 11:40:43 +0800

    面试题29. 顺时针打印矩阵

---
+++ cracking_the_coding_interview_6th/面试题29. 顺时针打印矩阵.go
@@ -0,0 +1,59 @@
+package cracking_the_coding_interview_6th
+
+/*
+* 面试题29. 顺时针打印矩阵
+* https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/
+ */
+
+func spiralOrder(matrix [][]int) []int {
+	if len(matrix) == 0 {
+		return nil
+	}
+	if len(matrix[0]) == 0 {
+		return nil
+	}
+
+	u := 0
+	d := len(matrix) - 1
+
+	l := 0
+	r := len(matrix[0]) - 1
+
+	var ans []int
+
+	for {
+		for i := l; i <= r; i++ {
+			ans = append(ans, matrix[u][i])
+		}
+
+		if u++; u > d {
+			break
+		}
+
+		for i := u; i <= d; i++ {
+			ans = append(ans, matrix[i][r])
+		}
+
+		if r--; r < l {
+			break
+		}
+
+		for i := r; i >= l; i-- {
+			ans = append(ans, matrix[d][i])
+		}
+
+		if d--; d < u {
+			break
+		}
+
+		for i := d; i >= u; i-- {
+			ans = append(ans, matrix[i][l])
+		}
+
+		if l++; l > r {
+			break
+		}
+	}
+
+	return ans
+}
---
+++ cracking_the_coding_interview_6th/面试题29. 顺时针打印矩阵_test.go
@@ -0,0 +1,19 @@
+package cracking_the_coding_interview_6th
+
+import (
+	. "github.com/kuhufu/leetcode"
+	"testing"
+)
+
+func Test_spiralOrder(t *testing.T) {
+	Run(t, spiralOrder, []Test{
+		{
+			Args{Str22DSlice("[[1,2,3,4],[5,6,7,8],[9,10,11,12]]")},
+			Want{Str2Slice("[1,2,3,4,8,12,11,10,9,5,6,7]")},
+		},
+		{
+			Args{Str22DSlice("[[1,2,3],[4,5,6],[7,8,9]]")},
+			Want{Str2Slice("[1,2,3,6,9,8,7,4,5]")},
+		},
+	})
+}
