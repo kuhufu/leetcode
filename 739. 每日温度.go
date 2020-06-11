2fe4e2bce681d61313a20d715c64ea4f29ee36ac
kuhufu <huboemail@yeah.net>
2020-06-11 16:45:52 +0800

    739. 每日温度

--- 739. 每日温度.go
+++ 739. 每日温度.go
@@ -7,7 +7,7 @@ package leetcode
 
 func dailyTemperatures(T []int) []int {
 	ans := make([]int, len(T))
-	var stack []int
+	stack := make([]int, 0, len(T))
 
 	for i := 0; i < len(T); i++ {
 		for len(stack) != 0 && T[stack[len(stack)-1]] < T[i] { //当前气温高于栈顶气温
