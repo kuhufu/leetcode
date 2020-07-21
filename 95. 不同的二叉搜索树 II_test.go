99edd5d17b52e78751932b4e73eae17505cefa8e
kuhufu <huboemail@yeah.net>
2020-07-21 10:57:40 +0800

    update

--- 95. 不同的二叉搜索树 II_test.go
+++ 95. 不同的二叉搜索树 II_test.go
@@ -7,11 +7,11 @@ func Test_generateTrees(t *testing.T) {
 		{
 			Args{3},
 			Want{[]*TreeNode{
-				Str2Tree("[1,null,3,2]"),
-				Str2Tree("[3,2,null,1]"),
-				Str2Tree("[3,1,null,null,2]"),
-				Str2Tree("[2,1,3]"),
 				Str2Tree("[1,null,2,null,3]"),
+				Str2Tree("[1,null,3,2]"),
+				Str2Tree("[2,1,3]"),
+				Str2Tree("[3,1,null,null,2]"),
+				Str2Tree("[3,2,null,1]"),
 			},
 			},
 		},
